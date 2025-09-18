6de2abf0388cebc0fb341cc2a9a91ef3f99ff637
Evelina Olsson <evol2401@student.miun.se>
2025-09-18 18:23:07 +0200

    Added Course interface with datatype definition for code, name, progression and URL

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import './style.css'
 
-
-
+//Interface that defines the datatypes for each field in a course
+interface Course {
+  code: string;
+  name: string;
+  progression: "A" | "B" | "C";
+  url: string;
+}
 
