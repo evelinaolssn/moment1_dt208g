239f28892edd2c64234be0ba50b6cbc53eba63a0
Evelina Olsson <evol2401@student.miun.se>
2025-09-18 18:50:09 +0200

    Added base function to add courses with interface validation

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,13 @@ interface Course {
   url: string;
 }
 
+//Function that creates a new Course object using the users provided input values
+function addCourse(
+  code: string,
+  name: string,
+  progression: "A" | "B" | "C",
+  url: string): Course {
+  return {
+    code, name, progression, url
+  };
+}
